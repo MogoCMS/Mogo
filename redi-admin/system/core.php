08a8bf5ebd3fd813367b0708f0add8734f022b78
Mogo <russellharrower@ZT903.local>
2014-10-19 18:22:38 +1100

    Save menu change

--- redi-admin/system/core.php
+++ redi-admin/system/core.php
@@ -5,18 +5,18 @@ global $M;
 
 class syscore {
 	static public $_db;
-	public static function db() 
+	public static function db()
 	{
    	 if (!static::$_db) {
         static::$_db = new MongoClient("mongodb://".SITEDBUSERNAME.":".SITEDBPASS."@".SITEDBURL.":27017/".SITEDB);
-		 
-		 
+
+
    	 }
 	 $siteDB = SITEDB;
    	 return static::$_db->$siteDB;
 	}
-	
-	
+
+
 	public function settings()
 	{
 		$collection = static::db()->settings;
@@ -24,25 +24,25 @@ class syscore {
 		if ($cursor->count() > 0)
 		{
 			// iterate through the results
-			while( $cursor->hasNext() ) {	
+			while( $cursor->hasNext() ) {
 			foreach($cursor->getNext() as $key => $value)
 				{
 					$key = ucwords($key);
-					define($key, $value);	
+					define($key, $value);
 				}
 			}
 		}
-		
+
 		//print siteName;
-		
+
 	}
-	
+
 	public function createuser($type){
-	
+
 		$data = $_POST;
 		$salt = self::gen_salt();
 		$collection = static::db()->salts;
-		$collection->insert(array("salt"=>$salt));		
+		$collection->insert(array("salt"=>$salt));
 		$new_salt = $collection->findOne(array("salt"=>$salt));
 		$password =  hash('sha512', $salt.$data['password'].$salt, FALSE);
 		$collection = static::db()->members;
@@ -58,7 +58,7 @@ class syscore {
 			)
 		);
 	}
-	
+
 	function gen_salt()
 	{
 		$a = substr(str_shuffle(str_repeat("0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ@!#$%^&*()_+=-", 20)), 0, 20);
@@ -66,29 +66,29 @@ class syscore {
 		$c = substr(str_shuffle(str_repeat("0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ@!#$%^&*()_+=-", 20)), 0, 20);
 		return $b.rand(1781,187156715671567156715761576157614652456246).$a.substr(md5(microtime()),rand(0,26),40).$c;
 	}
-	
-	
-	
+
+
+
 	function loginproccess(){
-		$collection = static::db()->members;	
-		$user = $collection->findOne(array("username"=>$_POST['username']));	
+		$collection = static::db()->members;
+		$user = $collection->findOne(array("username"=>$_POST['username']));
 		$collection = static::db()->salts;
 		$salt = $collection->findOne(array("_id"=>new MongoId($user['salt'])));
 		$password =  hash('sha512', $salt['salt'].$_POST['password'].$salt['salt'], FALSE);
-		
+
 		if($user['password'] == $password){
-			$collection = static::db()->members;	
+			$collection = static::db()->members;
 			$cursor = $collection->find(array("_id"=>new MongoId($user['_id'])),
 			array("_id","profile.picture","mydetails.name.first","mydetails.name.last"));
 			if ($cursor->count() == 1)
 			{
     			$test = array();
 				// iterate through the results
-				while( $cursor->hasNext() ) {	
+				while( $cursor->hasNext() ) {
 					$test[] = ($cursor->getNext());
 				}
 			}
-			//Print Results	
+			//Print Results
 			foreach($test as $d) {
     			$_SESSION["user"] = rand(178116717717,187156715671567156715761576157614652456246);
 			}
@@ -99,18 +99,18 @@ class syscore {
 		{
 			header("Location: index.php?error=wrongpassword");
 		}
-		
+
 	}
-	
+
 	/* Start Of Menus */
 	function fetch_menus(){
-		$menus = array();	
-		$collection = static::db()->menu;	
+		$menus = array();
+		$collection = static::db()->menu;
 		$cursor = $collection->find()->sort(array('order'=>1));
 		if ($cursor->count() > 0)
 		{
 			// iterate through the results
-			while( $cursor->hasNext() ) {	
+			while( $cursor->hasNext() ) {
 					$menus[] = ($cursor->getNext());
 			}
 		}
@@ -120,7 +120,7 @@ class syscore {
 		}
 		return $menus;
 	}
-	
+
 	function create_menu(){
 		$collection = static::db()->menu;
 		if($_POST['submenu'] == 0)
@@ -132,7 +132,7 @@ class syscore {
 		}
 	}
 	/* End of Menus*/
-	
+
 	/*Tips*/
 	function loadtip($area){
 		$tips = array();
@@ -141,7 +141,7 @@ class syscore {
 		if ($cursor->count() > 0)
 		{
 			// iterate through the results
-			while( $cursor->hasNext() ) {	
+			while( $cursor->hasNext() ) {
 					$tips[] = ($cursor->getNext());
 			}
 		}
@@ -149,19 +149,19 @@ class syscore {
 		{
 			var_dump(static::db()->lastError());
 		}
-		
+
 		return $tips;
 	}
 	/*end tips*/
 	/* Start of Categories */
 	function fetch_categories(){
-		$category = array();	
-		$collection = static::db()->categories;	
+		$category = array();
+		$collection = static::db()->categories;
 		$cursor = $collection->find();
 		if ($cursor->count() > 0)
 		{
 			// iterate through the results
-			while( $cursor->hasNext() ) {	
+			while( $cursor->hasNext() ) {
 					$category[] = ($cursor->getNext());
 			}
 		}
@@ -171,45 +171,45 @@ class syscore {
 		}
 		return $category;
 	}
-	
+
 	function create_category(){
-		$collection = static::db()->categories;	
+		$collection = static::db()->categories;
 		$cursor = $collection->find(array("category"=>ucwords($_POST['category'])));
 		if ($cursor->count() == 1)
 		{
 			return 1;
 		}
 		else {
-			
+
 		$collection->insert(array("category"=>ucwords($_POST['category'])));
 		return 0;
-		}	
+		}
 	}
-	
+
 	/* End of Categories */
 	/*Creating and Editing Pages/Post*/
 	function savepost(){
 		if($_POST['status'] == 'wait'){
 			$publish = $_POST['publishdate'];
-		}	
+		}
 		else
 		{
 			$publish = new MongoDate();
 		}
-			
+
 		$collection = static::db()->posts;
 		$collection->insert(array("title"=>htmlentities($_POST['title']),"url"=>$_POST['url'],"content"=>htmlentities($_POST['content']),"status"=>$_POST['status'],"date"=>$publish,"authorID"=>new MongoId($_POST['authorID']),"author"=>$_POST['author'],"category"=>$_POST['category'],"description"=>htmlentities($_POST['description']),"keywords"=>htmlentities($_POST['keywords']),"contenttype"=>$_POST["contenttype"]));
 	}
-	
+
 	function fetchdata($type){
-		$items = array();	
-		$collection = static::db()->posts;	
+		$items = array();
+		$collection = static::db()->posts;
 		$search = array("contenttype"=>$type);
 		$cursor = $collection->find($search);
 		if ($cursor->count() > 0)
 		{
 			// iterate through the results
-			while( $cursor->hasNext() ) {	
+			while( $cursor->hasNext() ) {
 					$items[] = ($cursor->getNext());
 			}
 		}
@@ -223,43 +223,43 @@ class syscore {
 	/*Start of menu creating*/
 	function savemenu(){
 		$collection = static::db()->menu;
-		if($_POST['menutask']== "url")
-		{
+		/*if($_POST['menutask']== "url")
+		{*/
 			$query = array("title"=>$_POST['title'],"menutask"=>$_POST['menutask'],"task"=>$_POST['task'],"menulevel"=>$_POST['menulevel']);
-			$collection->insert($query);		
-		}
+			$collection->insert($query);
+		/*}
 		else{
 			$query = array("_id"=>new MongoId($_POST['menulevel']));
 			$data = array('$push' => array("submenu"=>array("title"=>$_POST['title'],"menutask"=>$_POST['menutask'],"task"=>$_POST['task'])));
 			$options = array("upsert" => true);
-			$collection->update( $query, $data, $options );	
+			$collection->update( $query, $data, $options );
 		//$query = array("title"=>$_POST['title'],"menutask"=>$_POST['menutask'],"task"=>new MongoId($_POST['task']),"menulevel"=>$_POST['menulevel']);
-		}
-			
+	}*/
+
 	}
-	
+
 	function reordermenu(){
 		foreach($_POST['menu'] as $menu){
-			$collection = static::db()->menu;	
+			$collection = static::db()->menu;
 			$query = array("_id"=>new MongoId($menu['id']));
 			$data = array('$set' => array("order"=>$menu['order']));
 			$options = array("upsert" => true);
-			$collection->update( $query, $data, $options );	
-		
+			$collection->update( $query, $data, $options );
+
 		}
 	}
 	/*End of menu creating*/
-	
-	
-	
-	
-	
-	
-	
+
+
+
+
+
+
+
 	public function fetchmenuitem($task){
 		global $t;
 		include("templates/main/header.tpl");
-		
+
 		if(empty($_SESSION['user'])){
 			include("templates/main/login.tpl");
 		}
@@ -272,7 +272,7 @@ class syscore {
 					if($t=="pages" || $t=="posts" AND !empty($_POST)){
 						self::savepost();
 					}
-					
+
 					if($t == "pages"){
 					include("templates/main/create-pages.tpl");
 					}
@@ -283,12 +283,12 @@ class syscore {
 				break;
 				case 'media':
 					include("templates/main/media.tpl");
-					
+
 				break;
 				case 'menu':
 				if($_POST){
-					self::reordermenu();	
-				}	
+					self::reordermenu();
+				}
 					$menus = self::fetch_menus();
 					include("templates/main/menu.tpl");
 					$tip = self::loadtip("menuarea");
@@ -307,26 +307,26 @@ class syscore {
 				case 'menu/new':
 					if($_POST){
 						self::savemenu();
-					
+
 					$msg ="Your menu item has been added to the menu - please check out the menu section or continue adding another menu item below.";
-					
+
 					}
 					else
 					{
 					$msg ="Please choice the title of your menu item clearfully. As we use the SEO-Friendly URL that was created when your made the page you are now creating the menu link for. It is recommended that you choose the same name as the page for the menu title.";
-						
+
 					}
-					
+
 					$alert_color = "alert-warning";
 					include("templates/main/msg.tpl");
-					
+
 					$menus = self::fetch_menus();
 					$items = self::fetchdata('page');
 					include("templates/main/menunew.tpl");
 				break;
 				case 'categories':
 					if($_POST){
-						global $alert_color;	
+						global $alert_color;
 						$report = self::create_category();
 						if($report == 0)
 						{
@@ -341,9 +341,9 @@ class syscore {
 						include("templates/main/msg.tpl");
 						}
 					}
-					
+
 					$categories = self::fetch_categories();
-					
+
 					include("templates/main/categories.tpl");
 				break;
 				case 'manage':
@@ -357,18 +357,18 @@ class syscore {
 						$link = "create?t=posts";
 						$items = self::fetchdata('post');
 						include("templates/main/mpp.tpl");
-						
-					}			
+
+					}
 					break;
 				default:
-				 include("templates/main/home.tpl");	
+				 include("templates/main/home.tpl");
 				break;
 			}
 		}
-		
-		
+
+
 		include("templates/main/footer.tpl");
 	}
-	
+
 }
 ?>
